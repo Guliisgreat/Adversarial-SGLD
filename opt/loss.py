a510adfcba42705efb33d7f0d5593c5080a8b12a
guli <guliisgreat@hotmail.com>
2017-11-22 09:41:04 -0500

    add all monitor to train.py and check loss from nll to cross_e

--- opt/loss.py
+++ opt/loss.py
@@ -13,6 +13,7 @@ class CE(object):
         self.softmax = torch.nn.Softmax()
         self.nll = torch.nn.NLLLoss()
         self.CEL = torch.nn.CrossEntropyLoss()
+
     def CrossEntropyLoss(self, outputs, labels):
         return self.CEL(outputs.type(torch.FloatTensor), labels)
     def softmax_output(self, inputs):
@@ -30,7 +31,7 @@ class CE(object):
 
     def cross_entropy_loss(self, inputs, label):
         prob_inputs = self.softmax(inputs)
-        cross_e = self.CrossEntropyLoss(prob_inputs, label)
+        cross_e = self.CEL(prob_inputs, label)
         return cross_e
 
     def nll_loss(self, inputs, label):
@@ -51,19 +52,23 @@ class CE(object):
     def train(self, F, Y_batch):
         tv_F = Variable(F, requires_grad=True)
         tv_Y = Variable(torch.LongTensor(Y_batch.numpy().argmax(1)))
-        py_x = self.lsoftmax(tv_F)
-        loss = self.nll(py_x, tv_Y)
+        py_x = self.softmax(tv_F)
+        loss = self.CEL(py_x, tv_Y)
         ##
         loss.backward()
         G = tv_F.grad.data
         train_pred = py_x.data.numpy().argmax(1)
         return loss.data[0], G, train_pred
 
-    def infer(self, model, X_val, ret_proba=False):
+    def infer(self, model, X_val, Y_val, ret_proba=False):
         py_x = self.softmax(model.forward(X_val))
         proba = py_x.data.cpu().numpy()
         val_pred = proba.argmax(1)
+
+        tv_Y = Variable(torch.LongTensor(Y_val.data.numpy().argmax(1)))
+
+        ce_loss = self.CEL( py_x,tv_Y)
         if ret_proba:
-            return val_pred, proba
+            return val_pred, ce_loss, proba
         else:
-            return val_pred
+            return val_pred, ce_loss
